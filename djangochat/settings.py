57e27f56186b00ad79f7bd46e6fd09efd8efea7b
Ren Pena <rennielcpena@gmail.com>
2024-07-31 20:35:21 +0100

    Set up urls to view front page and added nav styling

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
     'core',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangochat.wsgi.application'
+ASGI_APPLICATION = 'djangochat.asgi.application'
 
 
 # Database
